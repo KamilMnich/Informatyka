9b0b9959479dd547f6b28c81e2aaa25867ff7cad
Kamil Mnich <kamimnichwp.pl@gmail.com>
2022-03-02 15:08:01 +0100

    NOWE

---
+++ Algortym liczby pierwszej/Algortym liczby pierwszej.cpp
@@ -0,0 +1,12 @@
+﻿// Algortym liczby pierwszej.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+}
+
