f8a041d411b4c02301533c5110c9a57e48d7a757
Kamil Mnich <kamimnichwp.pl@gmail.com>
2022-03-09 16:05:40 +0100

    lol

--- Algortym liczby pierwszej/Algortym liczby pierwszej.cpp
+++ Algortym liczby pierwszej/Algortym liczby pierwszej.cpp
@@ -7,6 +7,23 @@ using namespace std;
 int main()
 {
     int n;
+    int lp = 0;
+    int p = 2;
+    int d;
 
+    cout << "ile liczb pierwszych chcesz wyswietlic: ";
+    cin >> n;
+
+
+    while (lp < n);
+    {
+        for (int i = 0;  i <= n; i++)
+        {
+
+        }
+
+    }
+    
+    
 }
 
