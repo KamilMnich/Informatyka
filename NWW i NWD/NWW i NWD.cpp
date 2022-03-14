0ed1b03fe9e32759efbe44a81824584d2b8d6922
Kamil Mnich <kamimnichwp.pl@gmail.com>
2022-03-14 11:04:59 +0100

    Zadanie 2

---
+++ NWW i NWD/NWW i NWD.cpp
@@ -0,0 +1,33 @@
+﻿// NWW i NWD.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int iloczyn;
+
+	cout << "NWD" << endl;
+	    cout << "Podaj pierwsza liczbe: ";
+		cin >> a;
+		cout << "Podaj druga liczbe : ";
+		cin >> b;
+
+		while (a != b)
+		{
+			if ( a < b)
+			{
+				b = b - a;
+			}
+			else
+			{
+				a = a - b;
+			}
+		}
+		cout << "Najwiekszy wspolny dzielnik: " << a;
+	
+}
+
