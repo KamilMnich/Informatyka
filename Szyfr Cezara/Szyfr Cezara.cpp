e816544d4958c835d42c95546d184431d9c20c0d
Kamil Mnich <kamimnichwp.pl@gmail.com>
2022-03-28 11:05:40 +0200

    szyfr

---
+++ Szyfr Cezara/Szyfr Cezara.cpp
@@ -0,0 +1,21 @@
+﻿// Szyfr Cezara.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char tab[1001];
+
+    int klucz;
+
+    int dl;
+
+    cout << "Podaj wyraz skladajacy sie z malych liter: ";
+    cin >> tab;
+
+    cout << "Podaj klucz z przedzialu [-26..26]: ";
+    cin >> klucz;
+}
+
